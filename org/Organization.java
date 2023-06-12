d5d3f17c4f6d4fe84789492f068d64b72f9e8304
ajfinky <128330751+ajfinky@users.noreply.github.com>
2023-06-12 16:29:25 -0400

    Update Organization.java

--- org/Organization.java
+++ org/Organization.java
@@ -1,3 +1,8 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -36,5 +41,36 @@ public class Organization {
 		funds.add(fund);
 	}
 	
+	public List<Donation> getAllSortedDonations() {
+		
+		// initialize list of donations
+		List<Donation> donationList = new ArrayList<Donation>();
+		
+		for (Fund f : funds) {
+			for (Donation d : f.getDonations()) {
+				donationList.add(d);
+			}
+		}
+		
+		Comparator<Donation> dateCompare = new Comparator<Donation>() {
+			 SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
 
+	            @Override
+	            public int compare(Donation don1, Donation don2) {
+	                try {
+	                    Date date1 = dateFormat.parse(don1.getDate());
+	                    Date date2 = dateFormat.parse(don2.getDate());
+	                    return date2.compareTo(date1);  // Descending order
+	                } catch (Exception e) {
+	                    e.printStackTrace();
+	                }
+	                return 0;
+	            }
+		};
+		
+		Collections.sort(donationList, dateCompare);
+		
+		return donationList;
+	}
 }
+
