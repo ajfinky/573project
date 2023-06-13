0693209f463092f4d885befc4c1e0545777da39a
yinuotxie <yinuox@uw.edu>
2023-06-12 20:05:22 -0400

    finish 2.4 and 2.5

--- contributor/app/src/test/java/edu/upenn/cis573/project/DataManagerMakeDonationTest.java
+++ contributor/app/src/test/java/edu/upenn/cis573/project/DataManagerMakeDonationTest.java
@@ -9,39 +9,51 @@ import java.util.Map;
 
 public class DataManagerMakeDonationTest {
 
-    @Test
+    @Test(expected = IllegalStateException.class)
+    public void testNullWebClient() {
+        DataManager dm = new DataManager(null);
+        dm.makeDonation("123", "123", "123");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void testNullContributorId() {
-        DataManager dm = new DataManager(new WebClient(null, 0));
-        assertFalse(dm.makeDonation(null, "123", "10"));
+        DataManager dm = new DataManager(new WebClient("10", 3000));
+        dm.makeDonation(null, "123", "10");
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testNullFundId() {
-        DataManager dm = new DataManager(new WebClient(null, 0));
-        assertFalse(dm.makeDonation("123", null, "10"));
+        DataManager dm = new DataManager(new WebClient("10", 3000));
+        dm.makeDonation("123", null, "10");
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testNullAmount() {
-        DataManager dm = new DataManager(new WebClient(null, 0));
-        assertFalse(dm.makeDonation("123", "123", null));
+        DataManager dm = new DataManager(new WebClient("10", 3000));
+        dm.makeDonation("123", "123", null);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
+    public void testNonNumericAmount() {
+        DataManager dm = new DataManager(new WebClient("10", 3000));
+        dm.makeDonation("123", "123", "test");
+    }
+
+    @Test(expected = IllegalStateException.class)
     public void testUnsuccessfulStatus() {
-        DataManager dm = new DataManager(new WebClient(null, 0) {
+        DataManager dm = new DataManager(new WebClient("10", 3000) {
 
             @Override
             public String makeRequest(String resource, Map<String, Object> queryParams) {
                 return "{\"status\":\"unsuccessful\",\"data\":\"Snoopy\"}";
             }
         });
-        assertFalse(dm.makeDonation("123", "123", "123"));
+        dm.makeDonation("123", "123", "123");
     }
 
     @Test
     public void testSuccessfulStatus() {
-        DataManager dm = new DataManager(new WebClient(null, 0) {
+        DataManager dm = new DataManager(new WebClient("10", 3000) {
 
             @Override
             public String makeRequest(String resource, Map<String, Object> queryParams) {
@@ -49,5 +61,17 @@ public class DataManagerMakeDonationTest {
             }
         });
         assertTrue(dm.makeDonation("123", "123", "123"));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testMalformedJson() {
+        DataManager dm = new DataManager(new WebClient("10", 3000) {
+
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                return "{\"status1\":\"success\",\"data1\":\"Snoopy\"}";
+            }
+        });
+        dm.makeDonation("12345", "123", "123");
     }
 }
